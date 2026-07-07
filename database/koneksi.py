088049205f5e1da6887c0a0aebb1f2e7ddcbc680
Aandanu <90119516+Aandanu@users.noreply.github.com>
2026-07-07 21:35:44 +0700

    Update koneksi.py

--- database/koneksi.py
+++ database/koneksi.py
@@ -1,14 +1,10 @@
-import sqlite3
 import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "survey_sentimen.db")
+import psycopg
 
 def get_db():
-    conn = sqlite3.connect(
-        DB_PATH,
-        check_same_thread=False
+    conn = psycopg.connect(
+        os.environ["DATABASE_URL"],
+        autocommit=False
     )
-    conn.row_factory = sqlite3.Row
-    conn.execute("PRAGMA foreign_keys = ON")
+
     return conn
