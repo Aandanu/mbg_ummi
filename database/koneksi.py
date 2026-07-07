55b4a3239f70a117620a559617260aedca692a12
Aandanu <90119516+Aandanu@users.noreply.github.com>
2026-07-07 22:43:07 +0700

    Update koneksi.py

--- database/koneksi.py
+++ database/koneksi.py
@@ -3,7 +3,7 @@ import psycopg
 
 def get_db():
     conn = psycopg.connect(
-        os.environ["DATABASE_PUBLIC_URL"],
+        os.environ["DATABASE_URL"],
         autocommit=False
     )
     return conn
