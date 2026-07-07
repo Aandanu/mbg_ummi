b433d308dea442523b164d1c40c347e924a5f56d
Aandanu <90119516+Aandanu@users.noreply.github.com>
2026-07-07 22:00:36 +0700

    Update koneksi.py

--- database/koneksi.py
+++ database/koneksi.py
@@ -1,10 +1,12 @@
 import os
 import psycopg
 
+print("SEMUA ENV:", list(os.environ.keys()))
+print("DATABASE_URL:", os.getenv("DATABASE_URL"))
+
 def get_db():
     conn = psycopg.connect(
-        os.environ["DATABASE_URL"],
+        os.getenv("DATABASE_URL"),
         autocommit=False
     )
-
     return conn
